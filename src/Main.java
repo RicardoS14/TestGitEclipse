54827167da9aea063cc6d0c123c4b4920c53d7d9
Ricardo <sousaricardo844@gmail.com>
2018-11-07 09:11:29 +0100

    test 1

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	private static Scanner clavier;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		clavier = new Scanner(System.in);
+		
+		int nombre;
+		String nombre2;
+		
+		System.out.print("Nombre:");
+		nombre2 = clavier.nextLine();
+		
+		try{
+			nombre = Integer.parseInt(nombre2);
+			System.out.println(nombre*28);
+		}
+		catch(Exception erreur){
+			System.out.println("erreur de saisie");
+			System.out.println(erreur.toString());
+		}
+		
+	}
+
+}
